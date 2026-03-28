a2fe308e9d8434f3562a288b047d0929f3007a27
Ahmed Tamer <125506462+AhmedTamer025@users.noreply.github.com>
2026-03-29 00:33:04 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'pure-react-carousel/dist/react-carousel.es.css';
+// // import Icon from 'react-native-vector-icons/FontAwesome';
+// // import * as Animatable from 'react-native-animatable';
+// import Carousel from 'react-native-snap-carousel';
+import {
+  BrowserRouter,
+} from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
